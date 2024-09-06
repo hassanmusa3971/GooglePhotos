2ba019bf558d8b2e333b561284a87489f6169456
hassanmusa3971 <hassanmusa3971@gmail.com>
2024-09-06 02:34:45 -0700

    Pagination for local media

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,8 +6,10 @@ import { Image } from 'expo-image';
 
 export default function Home() {
   const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
-  const [assets, setAssets] = useState<MediaLibrary.Asset[]>([]);
-
+  const [localAssets, setLocalAssets] = useState<MediaLibrary.Asset[]>([]);
+  const [hasNextPage, setHasNextPage] = useState(true);
+  const [endCursor, setEndCursor] = useState<string>('');
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     if (permissionResponse?.status !== 'granted') {
       requestPermission();
@@ -16,25 +18,40 @@ export default function Home() {
 
   useEffect(() => {
     if (permissionResponse?.status === 'granted') {
-      loadLocalAssets();
+     loadLocalAssetsOnPage()
     }
   }, [permissionResponse]);
 
-  const loadLocalAssets = async () => {
-    const { assets, totalCount } = await MediaLibrary.getAssetsAsync();
-    setAssets(assets);
-    console.log(JSON.stringify(assets, null, 2));
-    console.log(totalCount);
+  const loadLocalAssetsOnPage = async() => {
+    if (loading || !hasNextPage) return;
+    setLoading(true);
+    const assetPage = await MediaLibrary.getAssetsAsync({ first: 30 })
+    setLocalAssets(assetPage.assets)
+    setHasNextPage(assetPage.hasNextPage);
+    setEndCursor(assetPage.endCursor);
+    setLoading(false);
+  }
+
+
+  const loadLocalAssets = async (assetId:string) => {
+    if(!hasNextPage || !assetId) return
+    const assetPage = await MediaLibrary.getAssetsAsync({after: assetId});
+    setLocalAssets((existingItems) => [...existingItems, ...assetPage.assets]);
+    setHasNextPage(assetPage.hasNextPage);
+    setEndCursor(assetPage.endCursor);   
   };
 
   return (
     <>
       <Stack.Screen options={{ title: 'Photos' }} />
       <FlatList
-        data={assets}
+        data={localAssets}
         numColumns={4}
         contentContainerClassName='gap-[2px]'
         columnWrapperClassName='gap-[2px]'
+        onEndReached={() =>loadLocalAssets(endCursor)}
+        onEndReachedThreshold={0.5}
+        refreshing={loading}
         renderItem={({ item }) => <Image source={{ uri: item.uri }} style={{ width: '25%', aspectRatio: 1}}/>}
         keyExtractor={(item) => item.id}
       />
