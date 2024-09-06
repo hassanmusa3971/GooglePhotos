a0209f836a3a5d4e8e895e6b751b2ea8e8a5642b
hassanmusa3971 <hassanmusa3971@gmail.com>
2024-09-05 18:51:57 -0700

    Load and display photos from media libery

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -12,7 +12,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Photos',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,43 @@
 import { Stack } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { FlatList, Text } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useState } from 'react';
+import { Image } from 'expo-image';
 
 export default function Home() {
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+  const [assets, setAssets] = useState<MediaLibrary.Asset[]>([]);
+
+  useEffect(() => {
+    if (permissionResponse?.status !== 'granted') {
+      requestPermission();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (permissionResponse?.status === 'granted') {
+      loadLocalAssets();
+    }
+  }, [permissionResponse]);
+
+  const loadLocalAssets = async () => {
+    const { assets, totalCount } = await MediaLibrary.getAssetsAsync();
+    setAssets(assets);
+    console.log(JSON.stringify(assets, null, 2));
+    console.log(totalCount);
+  };
+
   return (
     <>
       <Stack.Screen options={{ title: 'Photos' }} />
+      <FlatList
+        data={assets}
+        numColumns={4}
+        contentContainerClassName='gap-[2px]'
+        columnWrapperClassName='gap-[2px]'
+        renderItem={({ item }) => <Image source={{ uri: item.uri }} style={{ width: '25%', aspectRatio: 1}}/>}
+        keyExtractor={(item) => item.id}
+      />
     </>
   );
 }
