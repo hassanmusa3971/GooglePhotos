6d48eac4b4c2a38ef6e6ae95f822b2c7db2541c0
hassanmusa3971 <hassanmusa3971@gmail.com>
2024-09-08 00:56:10 -0700

    Imagekit integration

--- app/asset.tsx
+++ app/asset.tsx
@@ -3,6 +3,7 @@ import { useMedia } from './providers/MediaProvider';
 import { Text } from 'react-native';
 import { Image } from 'expo-image';
 import AntDesign from '@expo/vector-icons/AntDesign';
+import { getImagekitUrlFromPath } from '~/utils/imagekit';
 
 const AssetPage = () => {
   const { id } = useGlobalSearchParams<{id:string}>();
@@ -12,13 +13,14 @@ const AssetPage = () => {
   if (!asset) {
     return <Text>No Asset Found</Text>;
   }
+  const url = getImagekitUrlFromPath('23338271-3b23-49fe-9ea0-2f86052d3075/ELIZBETH%20FLIGHT-2.jpg',[{ width: 200, height: 200}])
   return (
     <>
     <Stack.Screen options={{ title: 'Photo', headerRight: () => {
       return (<AntDesign onPress={() => syncToCloud(asset)} name="cloudupload" size={24} color="black" />)
     }}} />
       <Image
-        source={{ uri: asset.uri }}
+        source={{ uri: url }}
         style={{ width: '100%', height: '100%' }}
         contentFit="contain"
       />
